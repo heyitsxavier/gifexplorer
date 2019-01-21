0a4708a435159e1acb055aa28f9aba972cc1f1e1
heyitsxavier <xavier.beynon@gmail.com>
2019-01-21 00:04:00 -0500

    New build

---
+++ docs/build/precache-manifest.d4c7a4bb0bc0e1147c8301d71b7e7872.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "430046be50ca44dec3c728457213be49",
+    "url": "./static/media/sammy.430046be.gif"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "22c9bc395af74637ba3a",
+    "url": "./static/js/main.22c9bc39.chunk.js"
+  },
+  {
+    "revision": "f01a2a340a8cf4dd5e38",
+    "url": "./static/js/1.f01a2a34.chunk.js"
+  },
+  {
+    "revision": "22c9bc395af74637ba3a",
+    "url": "./static/css/main.35bcdb3d.chunk.css"
+  },
+  {
+    "revision": "00dd5a3ddca35509526c278409499e79",
+    "url": "./index.html"
+  }
+];
