543187c4dabc6c529f33b961c50bd0adce1c57dd
heyitsxavier <xavier.beynon@gmail.com>
2019-01-20 23:26:52 -0500

    Try to set homepage to serve react from subdirectory

---
+++ docs/build/precache-manifest.d546f94986b327bb53b8324088ee2308.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "430046be50ca44dec3c728457213be49",
+    "url": "/gifexplorer/static/media/sammy.430046be.gif"
+  },
+  {
+    "revision": "6400e9a8d6afe078e085",
+    "url": "/gifexplorer/static/js/runtime~main.6400e9a8.js"
+  },
+  {
+    "revision": "935cec02538226cee707",
+    "url": "/gifexplorer/static/js/main.935cec02.chunk.js"
+  },
+  {
+    "revision": "8730cd67cbe176659984",
+    "url": "/gifexplorer/static/js/1.8730cd67.chunk.js"
+  },
+  {
+    "revision": "935cec02538226cee707",
+    "url": "/gifexplorer/static/css/main.a9a7ba9e.chunk.css"
+  },
+  {
+    "revision": "0c36121f2a24ad3dd6d58a95b91b26b3",
+    "url": "/gifexplorer/index.html"
+  }
+];
